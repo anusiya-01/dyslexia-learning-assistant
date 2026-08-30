8dbd2953956ded7cfe72a85c4059e7a7178054f1
anusiya-01 <anu04818@gmail.com>
2026-08-30 20:23:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function openReading() {
+    window.location.href = "http://127.0.0.1:5000/login";
+}
+
+function openPhonics() {
+    window.location.href = "http://127.0.0.1:5000/phonics";
+}
+    
+
+function openProgress() {
+    window.location.href = "/progress";
+}
+    
